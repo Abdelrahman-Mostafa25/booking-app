3b3046d9b42119bbaf9aebfbc35dad70a825e0e7
Abdelrahman-Mostafa25 <abdelrahman.mostafa2111@gmail.com>
2023-05-05 03:40:27 +0300

    hall_name_in_req_jason

--- app/Http/Controllers/RequstController.php
+++ app/Http/Controllers/RequstController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Requst\CreatRequst;
 use App\Models\Employee;
+use App\Models\Hall;
 use App\Models\Requst;
 use Illuminate\Http\Request;
 
@@ -54,7 +55,9 @@ class RequstController extends Controller
             if ($data) {
                 foreach ($data as $request) {
                     $employee = Employee::findOrFail($request->employee_num_id);
+                    $hall_name = Hall::findOrFail($request->hall_num);
                     $response = $request;
+                    $response['hall_name'] = $hall_name->hall_name;
                     $response['employee_email'] = $employee->email;
                     $response['employee_name'] = $employee->employee_name;
                     $responses[] = $response;
