19a808797e76a36459bfcec83287f50d3860574b
Abdelrahman-Mostafa25 <abdelrahman.mostafa2111@gmail.com>
2023-05-11 00:16:41 +0300

    modify_photos_post

--- app/Http/Controllers/Hall_photoController.php
+++ app/Http/Controllers/Hall_photoController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class Hall_photoController extends Controller
 {
-  /**
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -28,22 +28,27 @@ class Hall_photoController extends Controller
     {
         $request->validate([
             'hall_num_id' => 'required',
-             'photo' => 'required|image|mimes:jpg,png,jpeg,gif,svg',
+            'photos' => 'required|array',
+            'photos.*' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
         ]);
-        
-        $imageName = time().'.'.$request->photo->extension();
-        $path = 'image/hall_photos';
-        // Public Folder
-        $request->photo->move(public_path($path), $imageName);
 
-       
-        
-         Hall_photo::create([
-            'hall_num_id' => $request->hall_num_id,
-            'counter_id' => $request->counter_id,   
-            'photo' => $path.'/'.$imageName,
-         ]);
-    }// 
+        $path = 'image/hall_photos';
+        $uploaded_photos = [];
+
+        foreach ($request->photos as $photo) {
+            $imageName = time() . '.' . $photo->extension();
+            $photo->move(public_path($path), $imageName);
+            $uploaded_photos[] = $path . '/' . $imageName;
+        }
+
+        // do something with the $uploaded_photos array, like storing it in the database or processing it in some way
+
+        return response()->json([
+            'message' => 'Photos uploaded successfully',
+            'data' => $uploaded_photos
+        ]);
+    }
+
 
     /**
      * Display the specified resource.
@@ -53,8 +58,7 @@ class Hall_photoController extends Controller
      */
     public function show($id)
     {
-        return Hall_photo::where('hall_num_id',$id)->get();
-       
+        return Hall_photo::where('hall_num_id', $id)->get();
     }
 
     /**
@@ -65,17 +69,17 @@ class Hall_photoController extends Controller
      * @param  int  $id2
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,$id,$id2)
+    public function update(Request $request, $id, $id2)
     {
-        $hall_photo = Hall_photo::where('hall_num_id',$id)
-        ->where('counter_id',$id2)->get();
-        
+        $hall_photo = Hall_photo::where('hall_num_id', $id)
+            ->where('counter_id', $id2)->get();
+
         if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
-            $imageName = time().'.'.$request->photo->extension();
+            $imageName = time() . '.' . $request->photo->extension();
             $path = 'image/hall_photos';
             // Public Folder
             $request->photo->move(public_path($path), $imageName);
-             return $request;
+            return $request;
         } else {
             return 'test';
         }
@@ -84,13 +88,14 @@ class Hall_photoController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-    * @param  int  $id
+     * @param  int  $id
      * @param  int  $id2
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id,$id2)
-    {   $hall_supervisor =  Hall_photo::where('hall_num_id',$id)->where('counter_id',$id2);
+    public function destroy($id, $id2)
+    {
+        $hall_supervisor =  Hall_photo::where('hall_num_id', $id)->where('counter_id', $id2);
         $hall_supervisor->delete();
-        return response('',204);
+        return response('', 204);
     }
 }
