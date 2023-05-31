b967f0bad89b53ef19acd138b7b998dba169fe53
Abdelrahman-Mostafa25 <abdelrahman.mostafa2111@gmail.com>
2023-06-01 01:29:52 +0300

    delete_photo_employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -77,9 +77,10 @@ class EmployeeController extends Controller
             'password' => $inserted_employee->password,
             'phone_num' => $inserted_employee->phone_num,
             'specialization' => $inserted_employee->specialization,
-            'employee_photo' => $inserted_employee->employee_photo,
+            
         ];
         return response()->json($response);
+        // 'employee_photo' => $inserted_employee->employee_photo,
     }
 
 
