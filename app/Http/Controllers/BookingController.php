11f041f84b61204504762f2bb4ce875af71be3ee
Abdelrahman-Mostafa25 <abdelrahman.mostafa2111@gmail.com>
2023-06-05 02:33:07 +0300

    hall_report_course_name_error

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -264,21 +264,23 @@ class BookingController extends Controller
 
 
         $booking_data = DB::table('bookings')
-            ->where('hall_num_id','=' ,$hall_num_id)
-            ->orderBy('booking_day', 'asc')
-            ->orderBy('start_time_booking', 'asc')
-            ->get()->map(function ($booking) {
-                return [
-                    'Doctor' => DB::table('employees')->where('employee_id', $booking->employee_num_id)->first()->employee_name,
-                    'hall_name' => DB::table('halls')->where('hall_id', $booking->hall_num_id)->first()->hall_name,
-                    'type_hall' => $booking->type,
-                    'course_name' =>  DB::table('courses')->where('code', $booking->code)->first()->course_name,
-                    'booking_day' => $booking->booking_day,
-                    'start_time' => $booking->start_time_booking,
-                    'end_time' => $booking->end_time_booking,
-                ];
-            });
-
-        return $booking_data;
+        ->where('hall_num_id', '=', $hall_num_id)
+        ->orderBy('booking_day', 'asc')
+        ->orderBy('start_time_booking', 'asc')
+        ->get()
+        ->map(function ($booking) {
+            return [
+                'Doctor' => DB::table('employees')->where('employee_id', $booking->employee_num_id)->first()->employee_name,
+                'hall_name' => DB::table('halls')->where('hall_id', $booking->hall_num_id)->first()->hall_name,
+                'type_hall' => $booking->type,
+                'course_name' => DB::table('courses')->where('code', $booking->code)->first()->course_name ?? 'N/A',
+                'booking_day' => $booking->booking_day,
+                'start_time' => $booking->start_time_booking,
+                'end_time' => $booking->end_time_booking,
+            ];
+        });
+    
+    return $booking_data;
+    
     }
 }
